3bf50ea0a6cd0096c5514a3dada6af341c7e49a6
RyanRanjith <kingranjith4@gmail.com>
2024-11-10 15:16:39 -0800

    a

---
+++ String_Manipulation.py
@@ -0,0 +1,9 @@
+print("hello "+"Ranjith")
+print("hello" "   " + "Ranjith")
+
+
+# String manipulation excercise
+
+print("String Manipulation excercise ")
+print('String concatenation is done with "+" Sign ')
+print('e.g. print("Hello" + "Jenny")\nNew lines can be created with a backslash and n ')
