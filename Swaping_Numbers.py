c48dfffb8abcea5ccd3040bd17282a4b9ad9ce6a
RyanRanjith <kingranjith4@gmail.com>
2024-11-15 21:19:55 -0800

    a

---
+++ Swaping_Numbers.py
@@ -0,0 +1,24 @@
+# Excercise for swapping two numbers
+# My version
+
+# a = input("Enter value of a: ")
+# b = input("Enter value of b : ")
+
+# c = a
+# d = b
+
+# a = d
+# b = c
+
+# print(f"a = {a}" )
+# print(f"b = {b} ")
+
+# In Class
+a = input("Enter value of a: ")
+b = input("Enter value of b: ")
+temp = a
+a = b
+b = temp
+
+print("a = " + a)
+print("b =" + b)
