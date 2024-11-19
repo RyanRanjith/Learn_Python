32340475e546939ca4cb27b250a6e43f18c4762d
RyanRanjith <kingranjith4@gmail.com>
2024-11-19 09:22:22 -0800

    a

---
+++ Primitive_data_types.py
@@ -0,0 +1,28 @@
+# var_1 = 123
+# var_2 = 10.1
+# print(var_1 + var_2)
+# print(type(var_1))
+# print(type(var_2))
+
+# # in binary
+# var = 0o123
+# var2 = 0x123
+# print(var)
+# print(var2)
+
+# Strings
+
+# name = "Ranjith"
+# name_1 = "Varshini"
+# print(name + " " +   name_1)
+
+#  quotes inside string
+
+# print(" Ranjith\'s \"Good\" ")
+
+#  Using Boolean
+a = 2
+b = 1
+c = a < b
+print(c)
+print(type(c))
