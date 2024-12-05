936542984fc3559be5d7923da177d9489bb1dc29
RyanRanjith <kingranjith4@gmail.com>
2024-12-04 23:19:50 -0800

    a

--- BitWise_Operators.py
+++ BitWise_Operators.py
@@ -1,3 +1,17 @@
-a = 5
-b = 6
-print(a&b)
+# a = 2
+# b = 3
+# print(a&b)  # And Operator
+# print(a|b)    # OR Operator
+# print(a ^ b)  # XOR operator
+# print(~a)    # Compliment or NOT operator
+# print(a << 2)  # Left shift operator
+# print(a >> 2)  # Right shift operator# 
+
+#Excercise
+print(26&23)
+print(17 | 24)
+print(17 ^ 24)
+print(~45)
+print(68<<2)
+print(56>>3)
+
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Learn_Python
 BitWise operators starting on December 2
-
+Ended Lecture 17 December 4 
